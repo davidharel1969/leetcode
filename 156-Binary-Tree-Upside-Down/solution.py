dcda8ead905ed14c89c14adf1174608b613b1c9c
davidharel1969 <davidharel1969@hotmail.com>
2016-07-08 17:55:41 -0400

    [156. Binary Tree Upside Down][Accepted]committed by LeetCode Extension

---
+++ 156-Binary-Tree-Upside-Down/solution.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def upsideDownBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        p, parent, parent_right = root, None, None
+        
+        while p:
+            left = p.left
+            p.left = parent_right
+            parent_right = p.right
+            p.right = parent
+            parent = p
+            p = left
+            
+        return parent
+        
