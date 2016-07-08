b0d2bf00a3ed5a148494e58102c0fa2f3e0e1604
davidharel1969 <davidharel1969@hotmail.com>
2016-07-08 17:35:49 -0400

    [152. Maximum Product Subarray][Accepted]committed by LeetCode Extension

---
+++ 152-Maximum-Product-Subarray/solution.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxProduct(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxsofar=nums[0]
+        maxendinghere=nums[0]
+        minendinghere=nums[0]
+        print maxsofar,maxendinghere,minendinghere
+        for i,x in enumerate(nums[1:]):
+            maxendinghere,minendinghere=max(x,maxendinghere*x,minendinghere*x),min(x,maxendinghere*x,minendinghere*x)
+            
+            maxsofar=max(maxsofar,maxendinghere)
+            print x,maxsofar,maxendinghere,minendinghere
+        return maxsofar
+                
+                
