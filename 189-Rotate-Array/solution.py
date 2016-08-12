43b26bff670eed384357391fcb935aee23d22e4b
davidharel1969 <davidharel1969@hotmail.com>
2016-08-11 23:12:55 -0400

    [189. Rotate Array][Accepted]

---
+++ 189-Rotate-Array/solution.py
@@ -0,0 +1,20 @@
+def reverse(nums,left,right):
+    mid=(right-left)/2
+    for i in xrange(mid+1):
+        nums[left+i],nums[right-i]=nums[right-i],nums[left+i]
+    print left,right,mid,nums
+        
+
+
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        k=k%len(nums)
+        print k
+        reverse(nums,0,len(nums)-1)
+        reverse(nums,0,k-1)
+        reverse(nums,k,len(nums)-1)
